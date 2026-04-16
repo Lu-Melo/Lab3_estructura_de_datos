593372d4be0e747f21a53c33b80ff24e9e8fe629
Your Name <you@example.com>
2026-04-16 21:03:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,6 +74,7 @@ void insertMap(HashMap * map, char * key, void * value) {
             return;
         }
         pos++;
+        if (pos >) pos = 0;
     }
     map->buckets[pos] = par;
     map->current = pos;

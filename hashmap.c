42a864092577a92df62cf9a93a2e4a675a48eda1
Your Name <you@example.com>
2026-04-16 21:00:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,20 +68,16 @@ HashMap * createMap(long capacity) {
 void insertMap(HashMap * map, char * key, void * value) {
     long pos = hash(key, map->capacity);
     Pair *par = createPair(key, value);
-    
-    if(map->buckets[pos] == NULL) {
-        map->buckets[pos] = par;
-        map->current = pos;
-        map->size++;
-    } else {
-        while(map->buckets[pos] != NULL) {
-            if(is_equal(key, map->buckets[pos]->key) == 1) {
-                return;
-            }
-            pos = (pos+1)%map->capacity;
-        }    
+
+    while(map->buckets[pos] != NULL) {
+        if(is_equal(key, map->buckets[pos]->key) == 1) {
+            return;
+        }
+        pos++;
     }
-    
+    map->buckets[pos] = par;
+    map->current = pos;
+    map->size++;
 }
 
 // 3. Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
